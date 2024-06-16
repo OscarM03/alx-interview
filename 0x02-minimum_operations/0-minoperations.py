bc0fb8c61feb5e241fbebf35e67c733426f50a29
OscarM03 <oscarmutwiri03@gmail.com>
2024-06-16 20:02:55 +0300

    A function that get the minimum operations to execute only two operations in this file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Minimum Operation"""
+
+
+def minOperations(n):
+    """Get the minimum operations in a file"""
+    if n <= 0:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+
+        factor += 1
+
+    return operations
