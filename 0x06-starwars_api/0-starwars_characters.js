423ba4a227f7e498b6a4fd7a100a60446f89143e
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-12 00:20:40 +0300

    Fetching characters from an API:Star Wars Api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,35 @@
-#!/usr/bin/node
-// module
+#!/usr/bin/env node
 
 const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-const movieId = process.argv[2];
-const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const filmURL = `${API_URL}/films/${movieId}/`;
 
-
-function getMovieInfo () {
-  request(apiURL, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching movie info', error);
+  request(filmURL, (err, _, body) => {
+    if (err) {
+      console.error(err);
       return;
     }
-    if (response.statusCode !== 200) {
-      console.error(`Error: status code ${response.statusCode}`);
-      return;
-    }
-    const movieinfo = JSON.parse(body);
-    const charactersUrls = movieinfo.characters;
 
-    getCharacters(charactersUrls);
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (error, __, body) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(JSON.parse(body).name);
+        });
+      })
+    );
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.error(allErr));
   });
+} else {
+  console.log('Please provide a Movie ID as the first argument.');
 }
 
-function getCharacters (charactersUrls) {
-  charactersUrls.forEach(url => {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching character,', error);
-        return;
-      }
-      if (response.statusCode !== 200) {
-        console.error(`Error: status code: ${response.statusCode}`);
-        return;
-      }
-
-      const characterDetails = JSON.parse(body);
-      console.log(characterDetails.name);
-    });
-  });
-}
-
-getMovieInfo();
